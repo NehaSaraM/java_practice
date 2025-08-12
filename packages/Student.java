ff3116b7271661e1ddeeac3d73d5746177e1cac1
Neha Sara Mathews <neha.mathews12@gmail.com>
2025-08-12 22:57:54 +0530

    package

---
+++ packages/Student.java
@@ -0,0 +1,12 @@
+package studentinfo;
+public class Student{
+	private String name;
+	private int rollNo;
+	public Student(String name,int rollNo){
+		this.name=name;
+		this.rollNo=rollNo;
+		}
+	public void display(){
+		System.out.println("Name: "+name+" ,RollNO : "+rollNo);
+	}
+}
