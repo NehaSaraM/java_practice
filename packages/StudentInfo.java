360db47bbac382e80ebc7ef4b051a0af0106b470
Neha Sara Mathews <neha.mathews12@gmail.com>
2025-08-12 22:56:33 +0530

    package

---
+++ packages/StudentInfo.java
@@ -0,0 +1,10 @@
+import studentinfo.Student;
+public class StudentInfo{
+	public static void main(String[] args)
+{
+		Student s1=new Student("Neha Sara Mathews",14);
+		Student s2=new Student("Nayana",13);
+		s1.display();
+		s2.display();
+	}
+}
